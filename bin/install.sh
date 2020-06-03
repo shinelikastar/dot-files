a767bd73d7ac657a64ff4263dd624165b38a28c1
Star Su <starsu@stripe.com>
2020-06-03 14:28:32 -0700

    Read in private.zsh if it exists on shell startup

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+echo "setup .zshrc config"
 ln -sf ~/.dot-files/zshrc ~/.zshrc
+
+echo "setup .zsh directory"
+ln -sf ~/.dot-files/zsh ~/.zsh
--- zshrc
+++ zshrc
@@ -1,1 +1,3 @@
 echo HELLO
+
+[ -f ~/.zsh/private.zsh ] && source ~/.zsh/private.zsh
