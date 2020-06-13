8e2bf3e21ae18ad95e69437b982cdba70217ea71
Star Su <starsu@stripe.com>
2020-06-13 08:27:32 -0700

    colored ls

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,7 +1,14 @@
 # Navigation related
+alias ..="cd ../"
 alias ...="cd ../.."
 alias c="clear"
+
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias ls='ls -GFh'
 alias l="ls -la"
+
 alias dot="cd ~/.dot-files"
 alias 6="source ~/.zshrc; clear;"
 
