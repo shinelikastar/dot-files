b094d3c63786aca61c29706eda5958f369006b94
star <starsu@stripe.com>
2022-12-22 12:58:30 -0800

    split fzf into lua file

---
+++ nvim/lua/fzf.lua
@@ -0,0 +1,40 @@
+local actions = require('fzf-lua.actions')
+
+require('fzf-lua').setup({
+  fzf_opts = {
+    ['--layout'] = false,
+  },
+  files = {
+    actions = {
+      ["default"] = actions.file_edit_or_qf,
+      ["ctrl-x"] = actions.file_split,
+      ["ctrl-v"] = actions.file_vsplit,
+      ["ctrl-t"] = actions.file_tabedit,
+      ["alt-q"] = actions.file_sel_to_qf,
+    },
+    -- pay-server can't take the heat
+    file_icons = false,
+    git_icons = false,
+		raw_cmd = "rg --files --hidden --glob '!{node_modules/*,.git/*}'",
+  },
+  git = {
+    file_icons = false,
+    git_icons = false,
+  },
+  grep = {
+    file_icons = false,
+    git_icons = false,
+  },
+  winopts = {
+    preview = {
+      layout = 'vertical',
+    },
+  },
+})
+
+vim.cmd([[
+	noremap <C-b> :lua require('fzf-lua').buffers()<CR>
+	noremap <C-p> <cmd>lua require('fzf-lua').files()<CR>
+	noremap <space>fd <cmd>lua require('fzf-lua').grep()<CR>
+	noremap <space>fs <cmd>lua require('fzf-lua').grep_cword()<CR>
+]])
